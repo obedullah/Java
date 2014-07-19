902b8928ff4ca1fd5300fd68242498d9feed38d8
obedullah <obedullah93@gmail.com>
2014-07-20 04:07:48 +0530

    Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,21 +1,26 @@
+import java.util.Scanner;
+
 public class Factorial
-{
-	int n,result=1;
-	Factorial(int a)
+{	
+	int num,result=1;
+	Factorial(int n)
 	{
-		n=a;
+		num=n;
 	}
 	void fact()
 	{
-		for(int i=1;i<=n;i++)
+		for(int i=1;i<=num;i++)
 		{
 			result*=i;
 		}
-		System.out.println("the factorial value : "+result);
+		System.out.println("factorial value : "+result+"\n");
 	}
-	public static void main(String[]args)
+	public static void main(String [] args)
 	{
-		Factorial obj=new Factorial(7);
+		System.out.print("enter the number : ");
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		Factorial obj=new Factorial(n);
 		obj.fact();
 	}
 }
